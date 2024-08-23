608bd3a66dc90d9015b9648004a04d6318d58abe
stonxzz <cuentamlgpro@gmail.com>
2024-08-23 16:17:44 -0600

    feature :sparkles: An example was made of what routes are, how they work and how to assign callback functions to them to return anything when the route matches

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    return view('test');
+});
+
+Route::get('/crud', function () {
+    $age = 21;
+    $data = ['name' => 'Jorge', 'age' => $age];
+
+    return view('crud/index', $data);
+})->name('crud');//Ruta con nombre

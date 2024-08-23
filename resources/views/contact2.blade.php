1bd4510619990334d18e771b572ed71066b3ff14
stonxzz <cuentamlgpro@gmail.com>
2024-08-23 17:26:56 -0600

    feature :sparkles: A practice was done to create routes and with a callback function return a view, parameters were sent to it and with blade they were shown in the view

---
+++ resources/views/contact2.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact2</title>
+</head>
+<body>
+    <h3>Contact 2 view</h3>
+
+    <p>Edad: {{$age}}</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/contact', function () {
+    $data = ['name' => 'Stonxz'];
+    return view('contact', $data);
+})->name('contact');;
+
+
+Route::get('/contact2', function () {
+    return view('contact2', ['age' => '21']);
+})->name('contact2');
+
 Route::get('/test', function () {
     return view('test');
 });

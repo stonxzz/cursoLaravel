23b5ba8795898ad565940b8e3bc942e137cf97ac
stonxzz <cuentamlgpro@gmail.com>
2024-08-26 22:49:06 -0600

    feature :sparkles: The entire program was cleared for the new section

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('test', [PrimerControlador::class, 'index']);
-Route::get('otro/{post}/{otro?}', [PrimerControlador::class, 'otro']);
-
-
-Route::resource('post', PrimerControlador::class);

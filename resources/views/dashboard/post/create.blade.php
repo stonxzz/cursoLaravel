f1dbd139b770cb7e05695ff40d504e3e734a0333
stonxzz <cuentamlgpro@gmail.com>
2024-08-28 16:33:46 -0600

    feature: :sparkles: the mother view was created

--- resources/views/dashboard/post/create.blade.php
+++ resources/views/dashboard/post/create.blade.php
@@ -1,46 +1,39 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Crear post</title>
-</head>
-<body>
-    <h1>Crear Post</h1>
+@extends('dashboard.layout')
 
-    @include('dashboard.fragment._errors-form')
+@section('content')
+<h1>Crear Post</h1>
 
-    <form action="{{route('post.store')}}" method="post">
-        @csrf
+@include('dashboard.fragment._errors-form')
 
-        <label for="title">Title</label>
-        <input type="text" name="title">
+<form action="{{route('post.store')}}" method="post">
+    @csrf
 
-        <label for="slug">Slug</label>
-        <input type="text" name="slug">
-        
-        <label for="">Categoria</label>
-        <select name="category_id">
-            {{-- el title es la key y el id es el value que se manda en el post, asi funciona ese foreach --}}
-            @foreach ($categories as $title => $id)
-            <option value="{{$id}}">{{$title}}</option>
-            @endforeach
-        </select>
+    <label for="title">Title</label>
+    <input type="text" name="title">
 
-        <label for="">Posted</label>
-        <select name="posted">
-            <option value="not">No</option>
-            <option value="yes">Yes</option>
-        </select>
+    <label for="slug">Slug</label>
+    <input type="text" name="slug">
+    
+    <label for="">Categoria</label>
+    <select name="category_id">
+        {{-- el title es la key y el id es el value que se manda en el post, asi funciona ese foreach --}}
+        @foreach ($categories as $title => $id)
+        <option value="{{$id}}">{{$title}}</option>
+        @endforeach
+    </select>
 
-        <label for="content">Content</label>
-        <input type="text" name="content">
-        
-        <label for="description">Description</label>
-        <input type="text" name="description">
+    <label for="">Posted</label>
+    <select name="posted">
+        <option value="not">No</option>
+        <option value="yes">Yes</option>
+    </select>
 
-        <button type="submit">Enviar</button>
-    </form>
-</body>
-</html>
+    <label for="content">Content</label>
+    <input type="text" name="content">
+    
+    <label for="description">Description</label>
+    <input type="text" name="description">
+
+    <button type="submit">Enviar</button>
+</form>
+@endsection

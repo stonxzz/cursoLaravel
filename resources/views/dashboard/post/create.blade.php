581dfc1c0ec6830eb387dde98c624ab09a90a5e7
stonxzz <cuentamlgpro@gmail.com>
2024-08-28 16:29:34 -0600

    feature: :sparkles: A component was created to show errors

--- resources/views/dashboard/post/create.blade.php
+++ resources/views/dashboard/post/create.blade.php
@@ -9,13 +9,7 @@
 <body>
     <h1>Crear Post</h1>
 
-    @if ($errors->any())
-        @foreach ($errors->all() as $e)
-            <div class="error">
-                {{$e}}
-            </div>
-        @endforeach
-    @endif
+    @include('dashboard.fragment._errors-form')
 
     <form action="{{route('post.store')}}" method="post">
         @csrf

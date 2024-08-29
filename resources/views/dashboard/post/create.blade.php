c2f0d308613893b6705c2e0e7a17b81866cdd562
stonxzz <cuentamlgpro@gmail.com>
2024-08-28 18:59:11 -0600

    feature: :sparkles: It was configured in the create view to take the previous values ​​in case of an error

--- resources/views/dashboard/post/create.blade.php
+++ resources/views/dashboard/post/create.blade.php
@@ -9,30 +9,30 @@
     @csrf
 
     <label for="title">Title</label>
-    <input type="text" name="title">
+    <input type="text" name="title" value="{{old("title","sin titulo")}}">
 
     <label for="slug">Slug</label>
-    <input type="text" name="slug">
+    <input type="text" name="slug" value="{{old("slug","sin slug")}}">
     
     <label for="">Categoria</label>
     <select name="category_id">
         {{-- el title es la key y el id es el value que se manda en el post, asi funciona ese foreach --}}
         @foreach ($categories as $title => $id)
-        <option value="{{$id}}">{{$title}}</option>
+        <option {{ old("category_id","") == $id ? "selected" : ""}} value="{{$id}}">{{$title}}</option>
         @endforeach
     </select>
 
     <label for="">Posted</label>
     <select name="posted">
-        <option value="not">No</option>
-        <option value="yes">Yes</option>
+        <option {{ old("posted","") == "not" ? "selected" : ""}} value="not">No</option>
+        <option {{ old("posted","") == "yes" ? "selected" : ""}} value="yes">Yes</option>
     </select>
 
     <label for="content">Content</label>
-    <input type="text" name="content">
+    <input type="text" name="content" value="{{old("content","sin content")}}">
     
     <label for="description">Description</label>
-    <input type="text" name="description">
+    <input type="text" name="description" value="{{old("description","sin description")}}">
 
     <button type="submit">Enviar</button>
 </form>

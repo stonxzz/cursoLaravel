916ff826ea6ddaec83338c9213ac61bd8676a47f
stonxzz <cuentamlgpro@gmail.com>
2024-08-29 16:00:08 -0600

    feature: :sparkles: show view is set

--- app/Http/Controllers/Dashboard/PostController.php
+++ app/Http/Controllers/Dashboard/PostController.php
@@ -63,7 +63,7 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        //
+        return view("dashboard.post.show", compact('post'));
     }
 
     /**

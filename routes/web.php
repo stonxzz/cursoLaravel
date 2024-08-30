3dc20bf2eb0ac4bdcbd8c955d156a281e98dc05e
stonxzz <cuentamlgpro@gmail.com>
2024-08-30 14:08:04 -0600

    feature: :sparkles: Examples were made of how to use 3 types of routes: grouped, middleware and controller

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,42 @@
 use App\Http\Controllers\Dashboard\CategoryController;
 use App\Http\Controllers\Dashboard\PostController;
 use App\Http\Controllers\PrimerControlador;
+use App\Http\Middleware\TestMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-//Indica que un parametro es opcional
-Route::get('/test/{id?}/{name?}', function($id = 10, $name = "jorge"){
-    echo $id;
-    echo $name;
+Route::middleware([TestMiddleware::class])->group(function () {
+    Route::get('/test/{id?}/{name?}', function ($id = 10, $name = "jorge") {
+        echo $id;
+        echo $name;
+    });
 });
 
-Route::resource('post', PostController::class);
-Route::resource('category', CategoryController::class);
+Route::group(['prefix' => 'dashboard'], function () {
+    Route::resource('post', PostController::class);
+    Route::resource('category', CategoryController::class);
+});
+//Indica que un parametro es opcional
+// Route::get('/test/{id?}/{name?}', function ($id = 10, $name = "jorge") {
+//     echo $id;
+//     echo $name;
+// });
+
+// Route::controller(PostController::class)->group(function () {
+//     Route::get('post','index')->name("post.index");
+//     Route::get('post/{post}','show')->name("post.show");
+//     Route::get('post/create','create')->name("post.create");
+//     Route::get('post/{post}/edit','edit')->name("post.edit");
+
+//     Route::post('post','store')->name("post.store");
+//     Route::put('post/{post}/edit','update')->name("post.update");
+//     Route::delete('post/{post}/edit','delete')->name("post.destroy");
+// });
+
+
 
 // Route::get('post', [PostController::class, 'index']);
 // Route::get('post/{post}', [PostController::class, 'show']);

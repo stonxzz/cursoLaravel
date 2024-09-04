bb7e38960907fd67b2e5e7c1f6777b14f421026d
stonxzz <cuentamlgpro@gmail.com>
2024-09-04 16:40:36 -0600

    feature: :sparkles: Axios and caterpillar were configured

--- resources/js/vue/components/List.vue
+++ resources/js/vue/components/List.vue
@@ -1,5 +1,23 @@
+
+<script setup>
+
+const clickMe = () => {
+    alert("Clicked!");
+};
+
+
+
+</script>
+
 <template>
-    <div>
-        <h1>Listado de Post</h1>
-    </div>
+  <div>
+    <h1>Listado de Post</h1>
+    <o-field label="Email" variant="danger" message="This email is invalid">
+        <o-input type="email" value="john@" :maxlength="30" />
+    </o-field>
+    <o-button
+        @click="clickMe">
+        Click Me
+    </o-button>
+  </div>
 </template>
--- resources/js/vue/main.js
+++ resources/js/vue/main.js
@@ -1,6 +1,14 @@
 import { createApp } from "vue";
+import Oruga from '@oruga-ui/oruga-next';
+
+
+import axios from "axios";
+
 import App from "./App.vue";
 
-const app = createApp(App)
+const app = createApp(App).use(Oruga)
+
+app.config.globalProperties.$axios = axios
+window.axios = axios
 
 app.mount('#app')

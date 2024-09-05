23cde4a54df8ce4f53cf4c4bfd0ab7a4c90c53d6
stonxzz <cuentamlgpro@gmail.com>
2024-09-05 16:24:09 -0600

    feature: :sparkles: The request was made to create a post using Axios and sending the request to the API that was created in Laravel

--- resources/js/vue/components/Save.vue
+++ resources/js/vue/components/Save.vue
@@ -3,9 +3,16 @@ import { all } from "axios";
 
 export default {
   data() {
-    return{
-        categories: []
-    }
+    return {
+      categories: [],
+      form: {
+        title: "",
+        description: "",
+        content: "",
+        category_id: "",
+        posted: "",
+      },
+    };
   },
   mounted() {
     this.getCategory();
@@ -16,29 +23,41 @@ export default {
         this.categories = res.data;
       });
     },
+    submit(){
+        this.$axios.post("/api/post", this.form)
+        .then(res=>{
+            console.log(res)
+        })
+    }
   },
 };
 </script>
 
 <template>
-  <div>
-    <o-field label="Titulo">
-      <o-input value=""></o-input>
-    </o-field>
-    <o-field label="Descripcion">
-      <o-input type="textarea" value=""></o-input>
-    </o-field>
-    <o-field label="Categoria">
-      <o-select placeholder="Seleccione una categoria">
-        <option v-for="c in categories" :key="c.id" value="c.id">{{ c.title }}</option>
-      </o-select>
-    </o-field>
-    <o-field label="Posted">
-      <o-select placeholder="Seleccione un estado">
-        <option value="yes">Si</option>
-        <option value="not">No</option>
-      </o-select>
-    </o-field>
-    <o-button variant="primary">Enviar</o-button>
-  </div>
+    <!-- //El .prevent es para prevenir que se ejecute el evento por defecto de html -->
+  <form @submit.prevent="submit">
+    <div>
+      <o-field label="Titulo">
+        <input v-model="form.title" value=""></input>
+      </o-field>
+      <o-field label="Descripcion">
+        <input v-model="form.description" type="text-area" value=""></input>
+      </o-field>
+      <o-field label="Contenido">
+        <input v-model="form.content" type="text-area" value=""></input>
+      </o-field>
+      <o-field label="Categoria">
+        <o-select v-model="form.category_id" placeholder="Seleccione una categoria">
+          <option v-for="c in categories" :key="c.id" :value="c.id">{{ c.title }}</option>
+        </o-select>
+      </o-field>
+      <o-field label="Posted">
+        <o-select v-model="form.posted" placeholder="Seleccione un estado">
+          <option value="yes">Si</option>
+          <option value="not">No</option>
+        </o-select>
+      </o-field>
+      <o-button variant="primary" native-type="submit">Enviar</o-button>
+    </div>
+  </form>
 </template>

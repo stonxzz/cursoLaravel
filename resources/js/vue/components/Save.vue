968f1c477eb6ebd4e2bd339e3984600fe0dff0d3
stonxzz <cuentamlgpro@gmail.com>
2024-09-05 16:51:45 -0600

    feature: :sparkles: A function was created to display validation errors in the view

--- resources/js/vue/components/Save.vue
+++ resources/js/vue/components/Save.vue
@@ -12,21 +12,59 @@ export default {
         category_id: "",
         posted: "",
       },
+      errors: {
+        title: "",
+        description: "",
+        content: "",
+        category_id: "",
+        posted: "",
+      },
     };
   },
   mounted() {
     this.getCategory();
   },
   methods: {
+    cleanErrorsForm(){
+        this.errors.title = ""
+        this.errors.description = ""
+        this.errors.content = ""
+        this.errors.category_id = ""
+        this.errors.posted = ""
+    },
     getCategory() {
       this.$axios.get("/api/category/all").then((res) => {
         this.categories = res.data;
       });
     },
     submit(){
+        console.log(this.form)
+
+        this.cleanErrorsForm()
+
         this.$axios.post("/api/post", this.form)
         .then(res=>{
             console.log(res)
+        }).catch(e=>{
+            // es para accedr a los errores en especifico que ocurran
+            console.log(e.response.data)
+
+            if (e.response.data.title) {
+                this.errors.title = e.response.data.title[0]
+            }
+            if (e.response.data.description) {
+                this.errors.description = e.response.data.description[0]
+            }
+            if (e.response.data.content) {
+                this.errors.content = e.response.data.content[0]
+            }
+            if (e.response.data.category_id) {
+                this.errors.category_id = e.response.data.category_id[0]
+            }
+            if (e.response.data.posted) {
+                this.errors.posted = e.response.data.posted[0]
+            }
+
         })
     }
   },
@@ -37,21 +75,21 @@ export default {
     <!-- //El .prevent es para prevenir que se ejecute el evento por defecto de html -->
   <form @submit.prevent="submit">
     <div>
-      <o-field label="Titulo">
+      <o-field label="Titulo" :variant="errors.title ? 'danger' : primary" :message="errors.title">
         <input v-model="form.title" value=""></input>
       </o-field>
-      <o-field label="Descripcion">
+      <o-field label="Descripcion" :variant="errors.description ? 'danger' : primary" :message="errors.description">
         <input v-model="form.description" type="text-area" value=""></input>
       </o-field>
-      <o-field label="Contenido">
+      <o-field label="Contenido" :variant="errors.content ? 'danger' : primary" :message="errors.content">
         <input v-model="form.content" type="text-area" value=""></input>
       </o-field>
-      <o-field label="Categoria">
+      <o-field label="Categoria" :variant="errors.category_id ? 'danger' : primary" :message="errors.category_id">
         <o-select v-model="form.category_id" placeholder="Seleccione una categoria">
           <option v-for="c in categories" :key="c.id" :value="c.id">{{ c.title }}</option>
         </o-select>
       </o-field>
-      <o-field label="Posted">
+      <o-field label="Posted" :variant="errors.posted ? 'danger' : primary" :message="errors.posted">
         <o-select v-model="form.posted" placeholder="Seleccione un estado">
           <option value="yes">Si</option>
           <option value="not">No</option>
